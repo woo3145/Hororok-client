2bc96472a5e842fab26f281212ef7949567af44a
woo3145 <lcwoo3145@gmail.com>
2023-12-20 00:48:23 +0900

    ✨ - 라우팅 처리

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-import { Button } from '@/components/ui/button';
+import { ref, computed } from 'vue';
+import Home from './pages/Home.vue';
+import About from './pages/About.vue';
+import NotFound from './pages/NotFound.vue';
+
+const routes: { [key: string]: any } = {
+  '/': Home,
+  '/about': About,
+};
+
+const currentPath = ref(window.location.hash);
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash;
+});
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound;
+});
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
-  <Button>Click me</Button>
+  <a href="#/">Home</a> | <a href="#/about">About</a> |
+  <a href="#/non-existent-path">Broken Link</a>
+  <component :is="currentView" />
 </template>
