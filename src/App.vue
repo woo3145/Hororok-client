8a1399f68fca2e3c11493de82e62245cf943aa5c
woo3145 <lcwoo3145@gmail.com>
2023-12-20 01:23:42 +0900

    ✨ - Header component추가

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
+<template>
+  <Header />
+  <a href="#/">Home</a> | <a href="#/about">About</a> |
+  <a href="#/non-existent-path">Broken Link</a>
+  <component :is="currentView" />
+</template>
+
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 import Home from './pages/Home.vue';
 import About from './pages/About.vue';
 import NotFound from './pages/NotFound.vue';
+import Header from '@/components/Header.vue';
 
 const routes: { [key: string]: any } = {
   '/': Home,
@@ -19,9 +27,3 @@ const currentView = computed(() => {
   return routes[currentPath.value.slice(1) || '/'] || NotFound;
 });
 </script>
-
-<template>
-  <a href="#/">Home</a> | <a href="#/about">About</a> |
-  <a href="#/non-existent-path">Broken Link</a>
-  <component :is="currentView" />
-</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="flex items-center justify-between w-full py-3 px-8 shadow-md">
+    <a href="#/" class="text-xl font-semibold">Hororok</a>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage
+            src="https://github.com/radix-vue.png"
+            alt="@radix-vue"
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent class="w-56" align="end">
+        <DropdownMenuLabel class="font-normal flex">
+          <p class="text-sm font-medium leading-none">woo3145</p>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <a href="/#/profile">
+          <DropdownMenuItem class="cursor-pointer"> Profile </DropdownMenuItem>
+        </a>
+
+        <DropdownMenuItem class="text-destructive cursor-pointer">
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+</script>
